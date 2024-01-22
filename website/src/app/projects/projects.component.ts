aebc71c0ae70ea8ce8f6d47446c88dd06c998ae6
daedal00 <92826197+daedal00@users.noreply.github.com>
2024-01-22 11:48:46 -0800

    set up site

--- website/src/app/projects/projects.component.ts
+++ website/src/app/projects/projects.component.ts
@@ -2,11 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-projects',
-  standalone: true,
-  imports: [],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
+  projects = [
+    { title: 'Project 1', description: 'Description 1', link: 'link-to-project-1' },
+    { title: 'Project 2', description: 'Description 2', link: 'link-to-project-2' },
+    // Add more projects as needed
+  ];
 
+  constructor() { }
 }
